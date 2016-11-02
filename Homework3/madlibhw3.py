8b3d4bfdd318a5832188c5f97dd3a71ab538511d
tpickren92 <tpickren@gmail.com>
2016-11-02 16:10:26 -0400

    started madlibs

--- Homework3/madlibhw3.py
+++ Homework3/madlibhw3.py
@@ -11,5 +11,42 @@
 # 1) Print the new text
 print("START*******")
 
+import nltk 
+from nltk.book import *
+from nltk import bigrams
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk import word_tokenize, sent_tokenize
+import random #one of these is printing all the texts, is this ok?
+
+print("\n\n")
+print("type of text 2 is ", type(text2))
+print("Length of ",text2,"is",len(text2))
+print("Unique tokens in",text2,"are: ",len(set(text2)))
+
+
+tokens = nltk.word_tokenize(text2)
+print("TOKENS")
+print(tokens)
+tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+print("TAGGED TOKENS")
+print(tagged_tokens)
+if debug:
+	print ("First few tagged tokens are:")
+	for tup in tagged_tokens[:5]:
+		print (tup)
+
+
+# sentences = sent_tokenize(text2)
+# print ("Type of sentences is", type(sentences))
+print("\n\n")
+
+
+
+
+# tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
+# substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
+
+# for i in sentences:
+# 	print(i)
 
 print("\n\nEND*******")
