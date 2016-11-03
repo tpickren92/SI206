9b94e82d0d12b7ad425f9e9fe6d40aad35e25874
tpickren92 <tpickren@gmail.com>
2016-11-03 09:21:35 -0400

    started bs project and got it outputting the right text

--- Homework3/bshw3.py
+++ Homework3/bshw3.py
@@ -1,5 +1,5 @@
-# Use https://www.si.umich.edu/programs/bachelor-science-
-# information/bsi-admissions as a template.
+# Use https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions
+
 # STEPS 
 # Create a similar HTML file but 
 # 1) Replace every occurrence of the word “student” with “AMAZING
@@ -10,3 +10,30 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+
+import requests
+import re
+from bs4 import BeautifulSoup
+
+print
+print (" - ....working......")
+print
+
+
+base_url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, "html.parser")
+# print (soup)
+
+# for student in soup.findall('student'):
+# 	print (student.get('alt', 'No!!'))
+
+findstudent = soup.find_all(text = re.compile('student'))
+empty = []
+for word in findstudent:
+    fixed_text = str(word).replace('student', '“AMAZING student')
+    word.replace_with(fixed_text)
+    empty.append(fixed_text)
+
+print(empty)
