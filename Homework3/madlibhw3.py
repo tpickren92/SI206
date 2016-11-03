f2816a531fa315a257bd2f13e66dac9a82a4cf29
tpickren92 <tpickren@gmail.com>
2016-11-02 23:41:08 -0400

    added in mad lib generator code and tweaked to limit to 150 tokens

--- Homework3/madlibhw3.py
+++ Homework3/madlibhw3.py
@@ -3,11 +3,11 @@
 
 # Requirements:
 # 1) Only use the first 150 tokens
-# 2) Pick 5 parts of speech to prompt for, including nouns
+# 2) Pick 5 parts of speech to prompt for, including nouns - adjectives, verbs, adverbs, plural noun
 # 3) Replace nouns 15% of the time, everything else 10%
 
 # Deliverables:
-# 1) Print the orginal text (150 tokens)
+# 1) Print the orginal text (150 tokens) - done
 # 1) Print the new text
 print("START*******")
 
@@ -18,35 +18,40 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk import word_tokenize, sent_tokenize
 import random #one of these is printing all the texts, is this ok?
 
-print("\n\n")
-print("type of text 2 is ", type(text2))
-print("Length of ",text2,"is",len(text2))
-print("Unique tokens in",text2,"are: ",len(set(text2)))
+fname = "austen-sense.txt"
+f = open(fname, 'r')
+para = f.read()
 
-
-tokens = nltk.word_tokenize(text2)
+tokens = nltk.word_tokenize(para)
 print("TOKENS")
-print(tokens)
-tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+print(tokens[:151])
+
+tagged_tokens = nltk.pos_tag(tokens)
+tagged_tokens = tagged_tokens[:151] # gives us a tagged list of tuples
 print("TAGGED TOKENS")
 print(tagged_tokens)
-if debug:
-	print ("First few tagged tokens are:")
-	for tup in tagged_tokens[:5]:
-		print (tup)
 
 
-# sentences = sent_tokenize(text2)
-# print ("Type of sentences is", type(sentences))
-print("\n\n")
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","RB":"an adverb","JJ":"an adjective"}
+substitution_probabilities = {"NN":.15,"NNS":.15,"VB":.1,"RB":.1, "JJ":.1}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
 
 
-
-
-# tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
-# substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
-
-# for i in sentences:
-# 	print(i)
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+print
+print("final shit below")
+print ("".join(final_words))
 
 print("\n\nEND*******")
