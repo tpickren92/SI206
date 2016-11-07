5f34f355e3f7467a8de51ca08d10d14a2f5cc54e
tpickren92 <tpickren92@users.noreply.github.com>
2016-11-07 10:20:48 -0500

    Update twitterhw3a.py
    Redid to use tweepy

--- Homework3/twitterhw3a.py
+++ Homework3/twitterhw3a.py
@@ -7,42 +7,20 @@
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
 
-
+import tweepy
+import nltk
 import requests
 import requests_oauthlib
-import json
-import operator
 
+access_token = "36224295-icFWtb45o6oGSQlNow89Zon1dk3ezUPpfyoRZ9W1L"
+access_token_secret = "rb5sUyzZlQIbXQHnCoprBkxWdOethWw8goL0Qtc1A1onT"
+consumer_key = "Pixu7snuSdcO5wHpgPcFdZznu"
+consumer_secret = "3Oi9q76CZ9D5x0ULSdDKB1zydHqynTkPK1ockJCnEkwA5fRWlR"
 
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
 
-def twitterREST():
-    twitter_client_key = "TF0PtP0O9emktHIz6nE3gba3L" 
-    twitter_client_secret = '3GyhZPJbimHu3hfhmpE2nJw7VAZSHA78LL3QCon0TpGvw8j6nw'
-
-    twitter_oauth = requests_oauthlib.OAuth1Session(twitter_client_key, client_secret=twitter_client_secret)
-
-    search_params = {}
-    search_params['q'] = "#" + user_hashtag
-    search_params['count'] = 20
-    search_params['lang'] = 'en' 
-    search_params['result_type'] = 'mixed'
-
-    twitter_r = twitter_oauth.get("https://api.twitter.com/1.1/search/tweets.json", params = search_params) 
-
-    twitter_res = twitter_r.json()
-
-    #print "Caching Twitter data..."
-    f = open('nestedtwitter.txt', 'w') # create empty nestedtwitter.txt
-    f.write(json.dumps(twitter_res)) # load json formatted dictionary into nestedtwitter.txt
-    f.close()
-    #print 'Twitter data cached.'
-
-    fref = open("nestedtwitter.txt","r")
-    fstr = fref.read() # turn nestedtwitter.txt into single string
-    twitter_data = json.loads(fstr) # usuable json dictionary of all the cached data
-
-    test.testEqual(str(twitter_r), '<Response [200]>') # test if request is going through
-    test.testEqual(twitter_res, twitter_data) # test if both json dictionaries match
-
-    return twitter_data
-    
+api = tweepy.API(auth)
+img = "insert here"
+api.update_with_media(img, status="#UMSI-206 #Proj3")
+print("posted")
